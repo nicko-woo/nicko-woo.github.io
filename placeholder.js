4de6e54daebcc901424565460d75df44d08decdc
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-22 17:57:36 +0300

    asdfasdf

--- placeholder.js
+++ placeholder.js
@@ -18,7 +18,7 @@ var PlaceHolder = function ($scope, $element) {
     }
 }
 
-Core.PlaceHolderManager.register("PurchaseOrders_RightTopButtons", PlaceHolder)
+Core.PlaceHolderManager.register("SuppliersRegister_RightTopButtons", PlaceHolder)
 
 
 // var PlaceHolder = function ($scope, $element) {
