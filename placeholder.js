3fa791b0d2a7dcb88f57536677f90064bf14212b
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-23 13:00:17 +0300

    changed onClick

--- placeholder.js
+++ placeholder.js
@@ -22,9 +22,44 @@ var PlaceHolder = function ($scope, $element) {
     //     modal.style.display = "block";
     // }
 
-    this.onClick = function ($scope, $dialog) {
-        $dialog.dialog({}).open('app.html');
-        console.log('click-click');
+    // this.onClick = function ($scope, $dialog) {
+    //     $dialog.dialog({}).open('app.html');
+    //     console.log('click-click');
+    // }
+
+    $scope.onClick = function () {
+        var win = new wind({
+            moduleName: "PurchaseOrder",
+            windowName: "PurchaseOrderExtendedProperties",
+            width: "950px",
+            closeOnEscape: false,
+            closeOnBackDrop: false,
+            data: {
+                pkPurchaseId: $scope.purchaseOrder.pkPurchaseID,
+                ExtendedProperties: $scope.ExtendedProperties
+            }
+        }, self.options);
+        win.onWindowClosed = function (event) {
+            switch (event.action) {
+                case "OK":
+                    $scope.ExtendedProperties = event.result;
+                    $scope.CheckHasChanged();
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                    break;
+                case "CLOSE":
+                    if (event.result) {
+                        $scope.ExtendedProperties = event.result;
+                        $scope.CheckHasChanged();
+                        if (!$scope.$$phase) {
+                            $scope.$apply();
+                        }
+                    }
+                    break;
+            }
+        };
+        win.open();
     }
 }
 
