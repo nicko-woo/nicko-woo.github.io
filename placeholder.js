82264aedd08453911ddbb22e39fee478928d259c
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-24 00:26:31 +0300

    purchaseorder

--- placeholder.js
+++ placeholder.js
@@ -21,7 +21,7 @@ var PlaceHolder = function ($scope, $element) {
 
     this.onClick = function ($scope, $element) {
         var win = new wind({
-            moduleName: "PayUsingPayoneer",
+            moduleName: "PurchaseOrder",
             // windowName: "app",
             width: "600px",
             closeOnEscape: false,
