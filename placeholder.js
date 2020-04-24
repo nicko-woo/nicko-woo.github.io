2bb4dccb4c584f0a051cd121025f8226db7a6391
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-24 15:33:39 +0300

    PayWithPayoneerView.js

--- placeholder.js
+++ placeholder.js
@@ -22,7 +22,7 @@ var PlaceHolder = function ($scope, $element) {
     this.onClick = function ($scope, $element) {
         var win = new wind({
             moduleName: "PayWithPayoneer",
-            windowName: "PayWithPayoneerView",
+            windowName: "PayWithPayoneerView.js",
             width: "600px",
             closeOnEscape: false,
             closeOnBackDrop: false,
