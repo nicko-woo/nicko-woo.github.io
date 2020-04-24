5f6bbfd172459ecada7840aa6f801485e08569f4
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-24 16:08:53 +0300

    tgb

--- placeholder.js
+++ placeholder.js
@@ -21,7 +21,7 @@ var PlaceHolder = function ($scope, $element) {
 
     this.onClick = function ($scope, $element) {
         var win = new wind({
-            moduleName: "PayWithPayoneerView",
+            moduleName: "PayWithPayoneer",
             windowName: "PayWithPayoneerView.html",
             width: "600px",
             closeOnEscape: false,
