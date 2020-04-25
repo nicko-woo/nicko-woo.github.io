4bb9a188c20a74bac27ca846da05081bfd5edf51
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-26 01:26:52 +0300

    now working

--- placeholder.js
+++ placeholder.js
@@ -22,35 +22,16 @@ var PlaceHolder = function ($scope, $element) {
     this.onClick = function ($scope) {
         var win = new wind({
             moduleName: "PayWithPayoneer",
-            windowName: "PayWithPayoneerView",
+            windowName: "PayWithPayoneer",
             width: "900px",
+            height: "600px",
             closeOnEscape: false,
             closeOnBackDrop: false,
-            data: {},
-            content: '<div class="window"><div class="header-buttons pull-right"><button type="button" class="btn btn-primary header-button modal-close" key="CLOSE" handle-click="true" lw-tst="closeDialog"><i class="fa fa-times"></i></button></div><div class="title" handle-dblclick="true" key="FULLSCREEN"><h3 title="Purchase Order Extended Properties">Purchase Order Extended Properties</h3></div><div class="content"></div></div>'
+            data: {}
         });
-        // win.onWindowClosed = function (event) {
-        //     switch (event.action) {
-        //         case "OK":
-        //             $scope.ExtendedProperties = event.result;
-        //             $scope.CheckHasChanged();
-        //             if (!$scope.$$phase) {
-        //                 $scope.$apply();
-        //             }
-        //             break;
-        //         case "CLOSE":
-        //             if (event.result) {
-        //                 $scope.ExtendedProperties = event.result;
-        //                 $scope.CheckHasChanged();
-        //                 if (!$scope.$$phase) {
-        //                     $scope.$apply();
-        //                 }
-        //             }
-        //             break;
-        //     }
-        // };
+        
         win.open();
     }
 }
-// Core.PlaceHolderManager.register("MyInventory_EditInventoryItem_Tabs", PlaceHolder)
+
 Core.PlaceHolderManager.register("EditPurchaseOrder_RightTopButtons", PlaceHolder)
