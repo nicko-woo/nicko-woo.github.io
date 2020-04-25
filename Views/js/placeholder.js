a69673eb7336f4dee1494f9503fa1a1a9a706f28
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-26 02:39:31 +0300

    test

---
+++ Views/js/placeholder.js
@@ -0,0 +1,38 @@
+var PlaceHolder = function ($scope, $element) {
+    this.getItems = function () {
+        var items = [{
+            text: "Pay With Payoneer",
+            key: "PayWithPayoneerButton",
+            icon: "fa fa-credit-card",
+            content: {
+                moduleName: "PayWithPayoneer",
+                controlName: "PayWithPayoneer"
+            }
+        }];
+
+        return items;
+    }
+
+    this.isEnabled = function (itemKey) {
+        return true;
+    }
+
+    const wind = require('core/Window');
+
+    this.onClick = function ($scope) {
+        var win = new wind({
+            moduleName: "PayWithPayoneer",
+            windowName: "PayWithPayoneer",
+            title: "Pay with Payoneer - Order Test",
+            width: "900px",
+            height: "600px",
+            closeOnEscape: false,
+            closeOnBackDrop: false,
+            data: {}
+        });
+        
+        win.open();
+    }
+}
+
+Core.PlaceHolderManager.register("EditPurchaseOrder_RightTopButtons", PlaceHolder)
