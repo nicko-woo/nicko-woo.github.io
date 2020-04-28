978fd415e0b31743c3d5921b0d978a256208a1c0
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-28 22:05:39 +0300

    GRID

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -22,63 +22,82 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
     // }
 
 
-    $scope.init = function () {
+    // $scope.init = function () {
 
-        // $scope.gridScope.setItems($scope.items);
+    //     // $scope.gridScope.setItems($scope.items);
 
-        var grid;
-        var columns = [
-            { id: "title", name: "Title", field: "title" },
-            { id: "duration", name: "Duration", field: "duration" },
-            { id: "%", name: "% Complete", field: "percentComplete" },
-            { id: "start", name: "Start", field: "start" },
-            { id: "finish", name: "Finish", field: "finish" },
-            { id: "effort-driven", name: "Effort Driven", field: "effortDriven" }
-        ];
+    //     var grid;
+    //     var columns = [
+    //         { id: "title", name: "Title", field: "title" },
+    //         { id: "duration", name: "Duration", field: "duration" },
+    //         { id: "%", name: "% Complete", field: "percentComplete" },
+    //         { id: "start", name: "Start", field: "start" },
+    //         { id: "finish", name: "Finish", field: "finish" },
+    //         { id: "effort-driven", name: "Effort Driven", field: "effortDriven" }
+    //     ];
 
-        var options = {
-            enableCellNavigation: true,
-            enableColumnReorder: false
-        };
+    //     var options = {
+    //         enableCellNavigation: true,
+    //         enableColumnReorder: false
+    //     };
 
-        $(function () {
-            var data = [];
-            for (var i = 0; i < 5; i++) {
-                data[i] = {
-                    title: "Task " + i,
-                    duration: "5 days",
-                    percentComplete: Math.round(Math.random() * 100),
-                    start: "01/01/2009",
-                    finish: "01/05/2009",
-                    effortDriven: (i % 5 == 0)
-                };
-            }
-
-            grid = new Slick.Grid("#myGrid123", data, columns, options);
-        })
-
-        var gridScope = $element.find(".slickgrid").scope();
-
-        $('#myGrid123').on('shown', grid.resizeCanvas);
-        $("#myGrid123").children(".slick-viewport").css( "height", "300px" );
-    };
-
-    $scope.init();
-
-    // get payments
-    // let paymentHistory = function () {
-    //     $.ajax({
-    //         type: 'GET',
-    //         url: sessionStorage.getItem('getPaymentHistoryUrl'),
-    //         data: {},
-    //         headers: { 'Authorization': $scope.Token, 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Accept-Language': 'application/json' }
-    //     }).done(function (data) {
-    //         for (var i = 0; i < data.length; i++) {
-
+    //     $(function () {
+    //         var data = [];
+    //         for (var i = 0; i < 5; i++) {
+    //             data[i] = {
+    //                 title: "Task " + i,
+    //                 duration: "5 days",
+    //                 percentComplete: Math.round(Math.random() * 100),
+    //                 start: "01/01/2009",
+    //                 finish: "01/05/2009",
+    //                 effortDriven: (i % 5 == 0)
+    //             };
     //         }
 
-    //     });
+    //         grid = new Slick.Grid("#myGrid123", data, columns, options);
+    //     })
+
+    //     var gridScope = $element.find(".slickgrid").scope();
+
+    //     $('#myGrid123').on('shown', grid.resizeCanvas);
+    //     $("#myGrid123").children(".slick-viewport").css("height", "300px");
     // };
+
+    // $scope.init();
+
+    var dataView = new Slick.Data.DataView();
+
+    var containerEl = "myGrid123";
+
+    //Create columns
+    var columns = [
+        { id: "column1", name: "ID", field: "id" },
+        { id: "column2", name: "Language", field: "lang" },
+        { id: "column3", name: "Year", field: "year" }
+    ];
+
+    // Pass it as a data provider to SlickGrid.
+    var grid = new Slick.Grid(containerEl, dataView, columns, options);
+
+    // Make the grid respond to DataView change events.
+    dataView.onRowCountChanged.subscribe(function (e, args) {
+        grid.updateRowCount();
+        grid.render();
+    });
+
+    dataView.onRowsChanged.subscribe(function (e, args) {
+        grid.invalidateRows(args.rows);
+        grid.render();
+    });
+
+    var data = [
+        {'id': 'l1', 'lang': 'Java', 'year': 1995},
+        {'id': 'l2', 'lang': 'JavaScript', 'year': 1995},
+        {'id': 'l3', 'lang': 'C#', 'year': 2000},
+        {'id': 'l4', 'lang': 'Python', 'year': 1991}];
+      
+      // This will fire the change events and update the grid.
+      dataView.setItems(data);
 
 
 };
