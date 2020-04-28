3032f2b62f0e5574987fb6c830f044c695b39e8a
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-28 15:26:14 +0300

    asfdasdfasdf

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -23,7 +23,7 @@ var PayWithPayoneerView = function($scope, $element, $filter, $compile, $q, cont
     // }
 
     $scope.init = function () {
-        $scope.$parent.gridScope.setItems($scope.items);
+        $scope.$parent.gridScope.setItems($scope.items, "igpwp");
     };
 
     $scope.init();
