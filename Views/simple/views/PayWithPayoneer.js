bf7ae63d789ef636aaaf24414de1a3b691eafc2b
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-28 14:34:44 +0300

    trying..

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,6 +1,8 @@
 var PayWithPayoneerView = function($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService){
     console.log('pay with payoneer works!')
 
+    $scope.gridScope = null;
+
     $scope = $scope.$parent;
     $scope.items = $scope.$parent.gridScope.getItems();
 
@@ -10,36 +12,13 @@ var PayWithPayoneerView = function($scope, $element, $filter, $compile, $q, cont
     $scope.supplierList = [];
     
 
-    var grid;
-  var columns = [
-    {id: "title", name: "Title", field: "title"},
-    {id: "duration", name: "Duration", field: "duration"},
-    {id: "%", name: "% Complete", field: "percentComplete"},
-    {id: "start", name: "Start", field: "start"},
-    {id: "finish", name: "Finish", field: "finish"},
-    {id: "effort-driven", name: "Effort Driven", field: "effortDriven"}
-  ];
-
-  var options = {
-    enableCellNavigation: true,
-    enableColumnReorder: false
-  };
-
-  $(function () {
-    var data = [];
-    for (var i = 0; i < 500; i++) {
-      data[i] = {
-        title: "Task " + i,
-        duration: "5 days",
-        percentComplete: Math.round(Math.random() * 100),
-        start: "01/01/2009",
-        finish: "01/05/2009",
-        effortDriven: (i % 5 == 0)
-      };
+    function Initialize() {
+        // $scope.gridScope.subscribe("onDblClick", onDoubleClick);
+        // $scope.radioOnChanged($scope.buttons[0]);
+        let items = $scope.$parent.gridScope.getItems();
+        $scope.gridScope.setItems(items);
+        $scope.$apply();
     }
-
-    grid = new Slick.Grid("#myGrid", data, columns, options);
-  })
     
 
 
