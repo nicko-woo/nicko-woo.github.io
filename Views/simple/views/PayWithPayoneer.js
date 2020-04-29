e579f3ff5ce576edafb546248763043b6807c53e
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-29 17:25:12 +0300

    ZXC

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,5 +1,5 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService) {
-    console.log('pay with payoneer works124!')
+    console.log('pay with payoneer works125!')
 
     $scope.gridScope = null;
 
