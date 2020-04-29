91888cd6be3b8b2e9fbf31da0b3efaab0643ea53
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-04-30 01:31:25 +0300

    fixes

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,50 +1,54 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService) {
-    console.log('pay with payoneer works143!')
-
-    // $scope.gridScope = null;
+    console.log('pay with payoneer works144!')
 
     $scope = $scope.$parent;
     $scope.items = $scope.$parent.gridScope.getItems();
 
     $scope.testVar = $scope.$parent.purchaseOrder.pkPurchaseID;
     $scope.payments = [];
+    $scope.outstanding = null;
+    $scope.paid = null;
+    $scope.selectedToPay = null;
+    $scope.balance = null;
 
-    var pwpByItemsGrid;
-    var columns = [
-        { id: "column1", name: "SKU", field: "SKU" },
-        { id: "column2", name: "Ordered Quantity", field: "Quantity" },
-        { id: "column3", name: "Paid Quantity", field: "Quantity" },
-        { id: "column4", name: "Price", field: "UnitCost" },
-        { id: "column5", name: "Quantity To Pay", field: "Quantity" }
-    ];
+    // pay by items data grid
 
-    var options = {
-        enableCellNavigation: true,
-        enableColumnReorder: false,
-        enableAutoResize: true,
+    // var pwpByItemsGrid;
+    // var columns = [
+    //     { id: "column1", name: "SKU", field: "SKU" },
+    //     { id: "column2", name: "Ordered Quantity", field: "Quantity" },
+    //     { id: "column3", name: "Paid Quantity", field: "Quantity" },
+    //     { id: "column4", name: "Price", field: "UnitCost" },
+    //     { id: "column5", name: "Quantity To Pay", field: "Quantity" }
+    // ];
 
-    };
+    // var options = {
+    //     enableCellNavigation: true,
+    //     enableColumnReorder: false,
+    //     enableAutoResize: true,
 
-    $(function () {
-        var data = $scope.items;
-        pwpByItemsGrid = new Slick.Grid("#pwpByItemGrid", data, columns, options);
-        // $(".l0").addClass("slick-header-column")
+    // };
 
-        // pwpByItemsGrid.onScroll.subscribe(function () {
-        //     $(".l0").addClass("slick-header-column")
-        // })
-    })
+    // $(function () {
+    //     var data = $scope.items;
+    //     pwpByItemsGrid = new Slick.Grid("#pwpByItemGrid", data, columns, options);
+    //     // $(".l0").addClass("slick-header-column")
 
-      $scope.onInit = function () {
-        pwpByItemsGrid.invalidateAllRows();
-        pwpByItemsGrid.render();
-      };
+    //     // pwpByItemsGrid.onScroll.subscribe(function () {
+    //     //     $(".l0").addClass("slick-header-column")
+    //     // })
+    // })
 
-      $scope.onInit();
+    //   $scope.onInit = function () {
+    //     pwpByItemsGrid.invalidateAllRows();
+    //     pwpByItemsGrid.render();
+    //   };
+
+    //   $scope.onInit();
 
     // // // var dataView = new Slick.Data.DataView();
 
-    // // // var containerEl = "#myGrid123";
+    // // // var containerEl = "#pwpByItemGrid";
 
     // // // //Create columns
     // // // var columns = [
@@ -91,49 +95,4 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
 
     // // //   $scope.init();
 
-    // // //   $scope.refreshData = function() {
-    // // //     dataView.beginUpdate();
-    // // //     grid.invalidateAllRows();
-    // // //     dataView.setItems(data);
-    // // //     dataView.endUpdate();
-    // // //     grid.render();
-    // // //   }
-
-
-    // var grid;
-    // var columns = [{
-    //     id: "column",
-    //     name: "",
-    //     field: "column",
-    //     //selectable: false,
-    //     focusable: false
-    // },
-    // {
-    //     id: "data",
-    //     name: "Data",
-    //     field: "data"
-    // }
-    // ];
-
-    // var options = {
-    //     enableCellNavigation: true,
-    //     enableColumnReorder: false
-    // };
-
-    // $(function () {
-    //     var data = [];
-    //     for (var i = 0; i < 100; i++)
-    //         data[i] = {
-    //             column: "Column " + i,
-    //             data: i
-
-    //         };
-
-    //     grid = new Slick.Grid("#myGrid123", data, columns, options);
-    //     $(".l0").addClass("slick-header-column")
-
-    //     grid.onScroll.subscribe(function () {
-    //         $(".l0").addClass("slick-header-column")
-    //     })
-    // })
 };
