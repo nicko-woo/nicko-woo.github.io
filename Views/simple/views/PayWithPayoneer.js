6ce9702f7cdf737f32aae4c65530bc5af1f7d629
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-05-01 01:11:08 +0300

    fix

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,5 +1,5 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService) {
-    console.log('pay with payoneer works190!')
+    console.log('pay with payoneer works191!')
 
     $scope = $scope.$parent;
     $scope.orderItems = $scope.$parent.gridScope.getItems();
@@ -34,23 +34,44 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
     var containerEl = "#pwpByItemGrid";
 
     //Create columns
+    // var columns = [
+    //     { id: "column1", name: "SKU", field: "SKU", width: 160, cssClass: "slick-cell slickgrid-align-center" },
+    //     { id: "column2", name: "Ordered Quantity", field: "OrderedQuantity", width: 160, cssClass: "slick-cell slickgrid-align-center" },
+    //     { id: "column3", name: "Paid Quantity", field: "PaidQuantity", width: 140, cssClass: "slick-cell slickgrid-align-center" },
+    //     { id: "column4", name: "Price", field: "Price", width: 100, cssClass: "slick-cell slickgrid-align-center" },
+    //     { id: "column5", name: "Quantity To Pay", field: "ToPayQuantity", width: 160, editor: Slick.Editors.Text, cssClass: "slick-cell slickgrid-text-editor-icon slickgrid-align-center" }
+    // ];
+
     var columns = [
-        { id: "column1", name: "SKU", field: "SKU", width: 160, cssClass: "slick-cell slickgrid-align-center" },
-        { id: "column2", name: "Ordered Quantity", field: "OrderedQuantity", width: 160, cssClass: "slick-cell slickgrid-align-center" },
-        { id: "column3", name: "Paid Quantity", field: "PaidQuantity", width: 140, cssClass: "slick-cell slickgrid-align-center" },
-        { id: "column4", name: "Price", field: "Price", width: 100, cssClass: "slick-cell slickgrid-align-center" },
+        { id: "column1", name: "SKU", field: "SKU", width: 160 },
+        { id: "column2", name: "Ordered Quantity", field: "OrderedQuantity", width: 160 },
+        { id: "column3", name: "Paid Quantity", field: "PaidQuantity", width: 140 },
+        { id: "column4", name: "Price", field: "Price", width: 100 },
         { id: "column5", name: "Quantity To Pay", field: "ToPayQuantity", width: 160, editor: Slick.Editors.Text, cssClass: "slick-cell slickgrid-text-editor-icon slickgrid-align-center" }
     ];
+
+    for (var i in columns) {
+        if (i == 0) {
+            columns[i].cssClass = "slick-header-column slickgrid-align-center";
+            columns[i].formatter = defaultFormatter;
+        }
+        else {
+            if (!columns[i].editor) {
+                columns[i].cssClass = "slick-cell slickgrid-align-center";
+            }
+        }
+    }
+
 
     var options = {
         enableCellNavigation: true,
         enableColumnReorder: false,
         enableAutoResize: true,
-        editable: true
+        editable: true,
         // enableAddRow: true,
         // enableCellNavigation: true,
-        // asyncEditorLoading: false,
-        // autoEdit: false
+        asyncEditorLoading: false,
+        autoEdit: false
     };
 
     // Pass it as a data provider to SlickGrid.
