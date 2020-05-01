f16e52a9bf5c19f582ed63b7f483a142de4476a8
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-05-01 14:10:07 +0300

    test

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -92,8 +92,6 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
         grid.render();
     }
 
-    grid = new Slick.Grid(containerEl, dataView, columns, options);
-
     $scope.sumSelected = function (items, propA, propB) {
         return items.reduce(function (a, b) {
             return (a + (b[propA] * b[propB]));
@@ -143,5 +141,58 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
     // gridScope = grid.scope();
     // $scope.gridScope = gridScope;
     // $scope.gridScope.setItems($scope.orderItems);
+
+    function requiredFieldValidator(value) {
+        if (value == null || value == undefined || !value.length) {
+          return {valid: false, msg: "This is a required field"};
+        } else {
+          return {valid: true, msg: null};
+        }
+      }
+    
+      var testGrid;
+      var testData = [];
+      var testColumns = [
+        {id: "title", name: "Title", field: "title", width: 120, cssClass: "cell-title", editor: Slick.Editors.Text, validator: requiredFieldValidator},
+        {id: "desc", name: "Description", field: "description", width: 100, editor: Slick.Editors.LongText},
+        {id: "duration", name: "Duration", field: "duration", editor: Slick.Editors.Text},
+        {id: "%", name: "% Complete", field: "percentComplete", width: 80, resizable: false, formatter: Slick.Formatters.PercentCompleteBar, editor: Slick.Editors.PercentComplete},
+        {id: "start", name: "Start", field: "start", minWidth: 60, editor: Slick.Editors.Date},
+        {id: "finish", name: "Finish", field: "finish", minWidth: 60, editor: Slick.Editors.Date},
+        {id: "effort-driven", name: "Effort Driven", width: 80, minWidth: 20, maxWidth: 80, cssClass: "cell-effort-driven", field: "effortDriven", formatter: Slick.Formatters.Checkmark, editor: Slick.Editors.Checkbox}
+      ];
+      var testOptions = {
+        editable: true,
+        enableAddRow: true,
+        enableCellNavigation: true,
+        asyncEditorLoading: false,
+        autoEdit: false
+      };
+    
+      $(function () {
+        for (var i = 0; i < 500; i++) {
+          var d = (testData[i] = {});
+    
+          d["title"] = "Task " + i;
+          d["description"] = "This is a sample task description.\n  It can be multiline";
+          d["duration"] = "5 days";
+          d["percentComplete"] = Math.round(Math.random() * 100);
+          d["start"] = "01/01/2009";
+          d["finish"] = "01/05/2009";
+          d["effortDriven"] = (i % 5 == 0);
+        }
+    
+        testGrid = new Slick.Grid("#byAmountGrid", testData, testColumns, testOptions);
+    
+        testGrid.setSelectionModel(new Slick.CellSelectionModel());
+    
+        testGrid.onAddNewRow.subscribe(function (e, args) {
+          var item = args.item;
+          testGrid.invalidateRow(testData.length);
+          testData.push(item);
+          testGrid.updateRowCount();
+          testGrid.render();
+        });
+      })
 
 };
