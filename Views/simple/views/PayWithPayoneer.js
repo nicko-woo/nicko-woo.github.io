13f060433ea508e0ce11e0aa735ed6a366f2e5fd
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-05-04 02:14:30 +0300

    226

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,5 +1,5 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService) {
-    console.log('pay with payoneer works 225!')
+    console.log('pay with payoneer works 226!')
 
     $scope = $scope.$parent;
     $scope.orderItems = $scope.$parent.gridScope.getItems();
@@ -28,11 +28,10 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
         $scope.poItems.push(poItem);
     })
 
-    var dataView = new Slick.Data.DataView();
-    var containerEl = "#pwpByItemGrid";
+    var dataViewByItems = new Slick.Data.DataView();
 
-    // Create columns
-    var columns = [
+    // Create columnsByItems
+    var columnsByItems = [
         { id: "column1", name: "SKU", field: "SKU", width: 220, cssClass: "slick-cell slickgrid-align-center" },
         { id: "column2", name: "Ordered Quantity", field: "OrderedQuantity", width: 160, cssClass: "slick-cell slickgrid-align-center" },
         { id: "column3", name: "Paid Quantity", field: "PaidQuantity", width: 140, cssClass: "slick-cell slickgrid-align-center" },
@@ -40,7 +39,7 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
         { id: "column5", name: "Quantity To Pay", field: "ToPayQuantity", width: 160, editor: Slick.Editors.Text, cssClass: "slick-cell slickgrid-text-editor-icon slickgrid-align-center" }
     ];
 
-    // var columns = [
+    // var columnsByItems = [
     //     { id: "column1", name: "SKU", field: "SKU", width: 160 },
     //     { id: "column2", name: "Ordered Quantity", field: "OrderedQuantity", width: 160 },
     //     { id: "column3", name: "Paid Quantity", field: "PaidQuantity", width: 140 },
@@ -48,18 +47,18 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
     //     { id: "column5", name: "Quantity To Pay", field: "ToPayQuantity", width: 160, editor: Slick.Editors.Text, cssClass: "slick-cell slickgrid-text-editor-icon slickgrid-align-center" }
     // ];
 
-    // for (var i in columns) {
+    // for (var i in columnsByItems) {
     //     if (i == 0) {
-    //         columns[i].cssClass = "slick-header-column slickgrid-align-center";
+    //         columnsByItems[i].cssClass = "slick-header-column slickgrid-align-center";
     //     }
     //     else {
-    //         if (!columns[i].editor) {
-    //             columns[i].cssClass = "slick-cell slickgrid-align-center";
+    //         if (!columnsByItems[i].editor) {
+    //             columnsByItems[i].cssClass = "slick-cell slickgrid-align-center";
     //         }
     //     }
     // }
 
-    var options = {
+    var optionsByItems = {
         enableCellNavigation: true,
         enableColumnReorder: false,
         enableAutoResize: true,
@@ -70,26 +69,26 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
 
     $scope.dataByItems = $scope.poItems;
 
-    dataView.setItems($scope.dataByItems);
+    dataViewByItems.setItems($scope.dataByItems);
 
-    var grid = new Slick.Grid(containerEl, dataView, columns, options);
+    var gridByItems = new Slick.Grid("#pwpByItemGrid", dataViewByItems, columnsByItems, optionsByItems);
 
-    // grid.setSelectionModel(new Slick.CellSelectionModel());
+    // gridByItems.setSelectionModel(new Slick.CellSelectionModel());
 
-    // dataView.onRowCountChanged.subscribe(function (e, args) {
-    //     grid.updateRowCount();
-    //     grid.render();
+    // dataViewByItems.onRowCountChanged.subscribe(function (e, args) {
+    //     gridByItems.updateRowCount();
+    //     gridByItems.render();
     // });
 
-    // dataView.onRowsChanged.subscribe(function (e, args) {
-    //     grid.invalidateRows(args.rows);
-    //     grid.render();
+    // dataViewByItems.onRowsChanged.subscribe(function (e, args) {
+    //     gridByItems.invalidateRows(args.rows);
+    //     gridByItems.render();
     // });
 
     $scope.payByItems = function () {
         console.log("button works");
-        grid.invalidate();
-        grid.render();
+        gridByItems.invalidate();
+        gridByItems.render();
     }
 
     $scope.sumSelected = function (items, propA, propB) {
@@ -98,7 +97,7 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
         }, 0);
     };
 
-    grid.onCellChange.subscribe(
+    gridByItems.onCellChange.subscribe(
         function (e, args) {
             var tempSelectedToPay = 0;
             console.log('row: ' + args.row + ' cell: ' + args.cell);
@@ -107,17 +106,17 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
         });
 
     $scope.init = function () {
-        grid.resetActiveCell();
-        dataView.beginUpdate();
-        grid.invalidateAllRows();
-        dataView.setItems($scope.dataByItems);
-        dataView.endUpdate();
-        grid.render();
-        grid.updateRowCount();
-        grid.render();
-        grid.resizeCanvas();
-        grid.invalidate();
-        $('#pwpByItemGrid').on('shown', grid.resizeCanvas());
+        gridByItems.resetActiveCell();
+        dataViewByItems.beginUpdate();
+        gridByItems.invalidateAllRows();
+        dataViewByItems.setItems($scope.dataByItems);
+        dataViewByItems.endUpdate();
+        gridByItems.render();
+        gridByItems.updateRowCount();
+        gridByItems.render();
+        gridByItems.resizeCanvas();
+        gridByItems.invalidate();
+        $('#pwpByItemGrid').on('shown', gridByItems.resizeCanvas());
         $("#pwpByItemGrid").children(".slick-viewport").css( "height", "300px" );
 
         
@@ -125,103 +124,84 @@ var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, con
         $scope.selectedToPay = $scope.sumSelected($scope.poItems, 'Price', 'ToPayQuantity');
 
         // setTimeout(function () {
-        //     dataView.beginUpdate();
-        //     grid.invalidateAllRows();
-        //     dataView.setItems(data);
-        //     dataView.endUpdate();
-        //     grid.render();
+        //     dataViewByItems.beginUpdate();
+        //     gridByItems.invalidateAllRows();
+        //     dataViewByItems.setItems(data);
+        //     dataViewByItems.endUpdate();
+        //     gridByItems.render();
 
-        //     console.log("grid re-rendered after timeout")
+        //     console.log("gridByItems re-rendered after timeout")
         // }, 3000);
     };
 
     $scope.init();
 
     setTimeout(function () {
-        dataView.refresh();
-        grid = new Slick.Grid(containerEl, dataView, columns, options);
+        dataViewByItems.refresh();
+        gridByItems = new Slick.Grid(containerEl, dataViewByItems, columnsByItems, optionsByItems);
 
         console.log("dataview refreshed after timeout")
     }, 300);
 
+    // function requiredFieldValidator(value) {
+    //     if (value == null || value == undefined || !value.length) {
+    //       return {valid: false, msg: "This is a required field"};
+    //     } else {
+    //       return {valid: true, msg: null};
+    //     }
+    //   }
     
-
-    // var grid = $element.find(".slickgrid.pwpGrid");
-    // gridScope = grid.scope();
-    // $scope.gridScope = gridScope;
-    // $scope.gridScope.setItems($scope.orderItems);
-
-    function requiredFieldValidator(value) {
-        if (value == null || value == undefined || !value.length) {
-          return {valid: false, msg: "This is a required field"};
-        } else {
-          return {valid: true, msg: null};
-        }
-      }
+    //   var testGrid;
+    //   var testData = [];
+    //   var testColumns = [
+    //     {id: "title", name: "Title", field: "title", width: 120, cssClass: "cell-title", editor: Slick.Editors.Text, validator: requiredFieldValidator},
+    //     {id: "desc", name: "Description", field: "description", width: 100, editor: Slick.Editors.LongText},
+    //     {id: "duration", name: "Duration", field: "duration", editor: Slick.Editors.Text},
+    //     {id: "%", name: "% Complete", field: "percentComplete", width: 80, resizable: false, formatter: Slick.Formatters.PercentCompleteBar, editor: Slick.Editors.PercentComplete},
+    //     {id: "start", name: "Start", field: "start", minWidth: 60, editor: Slick.Editors.Date},
+    //     {id: "finish", name: "Finish", field: "finish", minWidth: 60, editor: Slick.Editors.Date},
+    //     {id: "effort-driven", name: "Effort Driven", width: 80, minWidth: 20, maxWidth: 80, cssClass: "cell-effort-driven", field: "effortDriven", formatter: Slick.Formatters.Checkmark, editor: Slick.Editors.Checkbox}
+    //   ];
+    //   var testOptions = {
+    //     editable: true,
+    //     enableAddRow: true,
+    //     enableCellNavigation: true,
+    //     asyncEditorLoading: false,
+    //     autoEdit: false
+    //   };
     
-      var testGrid;
-      var testData = [];
-      var testColumns = [
-        {id: "title", name: "Title", field: "title", width: 120, cssClass: "cell-title", editor: Slick.Editors.Text, validator: requiredFieldValidator},
-        {id: "desc", name: "Description", field: "description", width: 100, editor: Slick.Editors.LongText},
-        {id: "duration", name: "Duration", field: "duration", editor: Slick.Editors.Text},
-        {id: "%", name: "% Complete", field: "percentComplete", width: 80, resizable: false, formatter: Slick.Formatters.PercentCompleteBar, editor: Slick.Editors.PercentComplete},
-        {id: "start", name: "Start", field: "start", minWidth: 60, editor: Slick.Editors.Date},
-        {id: "finish", name: "Finish", field: "finish", minWidth: 60, editor: Slick.Editors.Date},
-        {id: "effort-driven", name: "Effort Driven", width: 80, minWidth: 20, maxWidth: 80, cssClass: "cell-effort-driven", field: "effortDriven", formatter: Slick.Formatters.Checkmark, editor: Slick.Editors.Checkbox}
-      ];
-      var testOptions = {
-        editable: true,
-        enableAddRow: true,
-        enableCellNavigation: true,
-        asyncEditorLoading: false,
-        autoEdit: false
-      };
+    //   $(function () {
+    //     for (var i = 0; i < 500; i++) {
+    //       var d = (testData[i] = {});
     
-      $(function () {
-        for (var i = 0; i < 500; i++) {
-          var d = (testData[i] = {});
+    //       d["title"] = "Task " + i;
+    //       d["description"] = "This is a sample task description.\n  It can be multiline";
+    //       d["duration"] = "5 days";
+    //       d["percentComplete"] = Math.round(Math.random() * 100);
+    //       d["start"] = "01/01/2009";
+    //       d["finish"] = "01/05/2009";
+    //       d["effortDriven"] = (i % 5 == 0);
+    //     }
+
+    //     setTimeout(function () {
+    //         testGrid = new Slick.Grid("#byAmountGrid", testData, testColumns, testOptions);
+
+    //         console.log("gridByItems re-rendered after timeout")
+    //     }, 500);
     
-          d["title"] = "Task " + i;
-          d["description"] = "This is a sample task description.\n  It can be multiline";
-          d["duration"] = "5 days";
-          d["percentComplete"] = Math.round(Math.random() * 100);
-          d["start"] = "01/01/2009";
-          d["finish"] = "01/05/2009";
-          d["effortDriven"] = (i % 5 == 0);
-        }
+    //     setTimeout(function () {
+    //         testGrid = new Slick.Grid("#byAmountGrid", testData, testColumns, testOptions);
 
-        setTimeout(function () {
-            testGrid = new Slick.Grid("#byAmountGrid", testData, testColumns, testOptions);
+    //         console.log("gridByItems re-rendered after timeout")
+    //     }, 500);
 
-            console.log("grid re-rendered after timeout")
-        }, 500);
-    
-        // testGrid = new Slick.Grid("#byAmountGrid", testData, testColumns, testOptions);
-    
-        // testGrid.setSelectionModel(new Slick.CellSelectionModel());
-    
-        // testGrid.onAddNewRow.subscribe(function (e, args) {
-        //   var item = args.item;
-        //   testGrid.invalidateRow(testData.length);
-        //   testData.push(item);
-        //   testGrid.updateRowCount();
-        //   testGrid.render();
-        // });
+    //     setTimeout(function () {
+    //         testGrid.invalidate();
 
-        setTimeout(function () {
-            testGrid = new Slick.Grid("#byAmountGrid", testData, testColumns, testOptions);
-
-            console.log("grid re-rendered after timeout")
-        }, 500);
-
-        setTimeout(function () {
-            testGrid.invalidate();
-
-            console.log("grid refreshed after timeout")
-        }, 500);
+    //         console.log("gridByItems refreshed after timeout")
+    //     }, 500);
 
 
-      })
+    //   })
 
 };
