5abe46244eb504c267a145acc9194b9399c2bcc6
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-05-05 01:13:50 +0300

    277

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,5 +1,5 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService, $http, $timeout) {
-    console.log('pay with payoneer works 276!')
+    console.log('pay with payoneer works 277!')
 
     var self = this;
     self.onMessage = function(msg) {
