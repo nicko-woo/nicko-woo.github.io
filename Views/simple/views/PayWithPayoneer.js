e0204cc417ba9135ce1193ef1e8a58b396946471
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-05-04 11:35:14 +0300

    239

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,5 +1,5 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService) {
-    console.log('pay with payoneer works 238!')
+    console.log('pay with payoneer works 239!')
 
     $scope = $scope.$parent;
     $scope.orderItems = $scope.$parent.gridScope.getItems();
