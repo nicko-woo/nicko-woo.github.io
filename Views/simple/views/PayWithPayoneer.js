f0418200b3771d5102d32b6e09f5aeff974108fb
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-05-05 11:24:45 +0300

    288

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,5 +1,5 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService, $http, $timeout) {
-    console.log('pay with payoneer works 287!')
+    console.log('pay with payoneer works 288!')
 
     // const SlickGridExtended = require("./SlickGridExtended");
 
