d03e650bd35bf9946fe6b4aa6c7bb151542c802b
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-05-05 20:42:21 +0300

    balance 297

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,5 +1,5 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService, $http, $timeout) {
-    console.log('pay with payoneer works 296!')
+    console.log('pay with payoneer works 297!')
 
     // const SlickGridExtended = require("./SlickGridExtended");
 
