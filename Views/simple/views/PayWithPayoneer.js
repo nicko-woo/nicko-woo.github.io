9ade4bf5cf935278e684b4189d34210fab1ba525
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-05-15 03:05:19 +0300

    397

--- Views/simple/views/PayWithPayoneer.js
+++ Views/simple/views/PayWithPayoneer.js
@@ -1,5 +1,5 @@
 var PayWithPayoneerView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService, $http, $timeout) {
-    console.log('pay with payoneer works 396!')
+    console.log('pay with payoneer works 397!')
 
     const apiUrl = "https://test-app-lp.azurewebsites.net/";
 
