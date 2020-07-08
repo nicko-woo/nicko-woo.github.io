fa4c74a6174e17a6e05abba096067056fdc07dfd
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-07-08 17:32:10 +0300

    clean

--- Views/simple/views/RemoveOutOfStock.js
+++ Views/simple/views/RemoveOutOfStock.js
@@ -1,9 +1,4 @@
 var RemoveOutOfStockView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService, $http, $timeout) {
-    console.log('RemoveOutOfStock works 106!');
-
-
-    $scope.removeOutOfStock = function () {
-        console.log("successfully removed");
-    };
+    
 
 };
