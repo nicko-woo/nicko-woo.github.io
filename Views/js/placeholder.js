1f39417f76f845fab6aac3c871e099bbf72231fa
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-07-08 03:07:58 +0300

    113

--- Views/js/placeholder.js
+++ Views/js/placeholder.js
@@ -26,10 +26,7 @@ var PlaceHolder = function ($scope, $element) {
                         case "YES":
                             $scope.itemChangedAskRecalculation(
                                 function () {
-                                    $scope.removeItemConfirmed(item, true);
-                                },
-                                function () {
-                                    $scope.removeItemConfirmed(item, false);
+                                    $scope.removeOutOfStock();
                                 }
                             );
                             break;
@@ -77,61 +74,61 @@ var PlaceHolder = function ($scope, $element) {
         });
     };
 
-    $scope.removeItem = function (item) {
-        //$scope.selectedItem = item;
-        if (item != null) {
-            var msg = "Are you sure you want to delete item \"{0} - {1}\"?".replace("{0}", item.SKU).replace("{1}", item.Title);
-            if (item.IsService)
-                msg = "Are you sure you want to delete \"{0}\"?".replace("{0}", item.Title);
+    // $scope.removeItem = function (item) {
+    //     //$scope.selectedItem = item;
+    //     if (item != null) {
+    //         var msg = "Are you sure you want to delete item \"{0} - {1}\"?".replace("{0}", item.SKU).replace("{1}", item.Title);
+    //         if (item.IsService)
+    //             msg = "Are you sure you want to delete \"{0}\"?".replace("{0}", item.Title);
 
-            dialogs.question({
-                message: msg,
-                title: "Delete?",
-                callback: function (event) {
-                    switch (event.action) {
-                        case "YES":
-                            $scope.itemChangedAskRecalculation(
-                                function () {
-                                    $scope.removeItemConfirmed1(item, true);
-                                },
-                                function () {
-                                    $scope.removeItemConfirmed1(item, false);
-                                }
-                            );
-                            break;
-                    }
-                }
-            }, self.options);
-        }
-    };
+    //         dialogs.question({
+    //             message: msg,
+    //             title: "Delete?",
+    //             callback: function (event) {
+    //                 switch (event.action) {
+    //                     case "YES":
+    //                         $scope.itemChangedAskRecalculation(
+    //                             function () {
+    //                                 $scope.removeItemConfirmed1(item, true);
+    //                             },
+    //                             function () {
+    //                                 $scope.removeItemConfirmed1(item, false);
+    //                             }
+    //                         );
+    //                         break;
+    //                 }
+    //             }
+    //         }, self.options);
+    //     }
+    // };
 
-    $scope.removeItemConfirmed1 = function (item, recalculatePackaging) {
-        var service = new Services.OrdersService(self.options);
-        busyWorker.showBusy($element.find(".itemsTableContainer"), "Updating");
-        service.removeOrderItem($scope.order.OrderId, item.RowId, $scope.locationId, function (event) {
-            if (event.hasErrors() == false) {
-                for (var i = 0 ; i < $scope.order.Items.length ; i++) {
-                    if ($scope.order.Items[i].RowId == item.RowId) {
-                        $scope.order.Items.splice(i, 1);
-                        item = null;
-                        break;
-                    }
-                }
-                if (recalculatePackaging) {
-                    $scope.saveOrderPackagingCalculation(true, false, function () {
-                        $scope.updateTotalsInfo(event.result);
-                    });
-                }
-                else
-                    $scope.updateTotalsInfo(event.result);
+    // $scope.removeItemConfirmed1 = function (item, recalculatePackaging) {
+    //     var service = new Services.OrdersService(self.options);
+    //     busyWorker.showBusy($element.find(".itemsTableContainer"), "Updating");
+    //     service.removeOrderItem($scope.order.OrderId, item.RowId, $scope.locationId, function (event) {
+    //         if (event.hasErrors() == false) {
+    //             for (var i = 0 ; i < $scope.order.Items.length ; i++) {
+    //                 if ($scope.order.Items[i].RowId == item.RowId) {
+    //                     $scope.order.Items.splice(i, 1);
+    //                     item = null;
+    //                     break;
+    //                 }
+    //             }
+    //             if (recalculatePackaging) {
+    //                 $scope.saveOrderPackagingCalculation(true, false, function () {
+    //                     $scope.updateTotalsInfo(event.result);
+    //                 });
+    //             }
+    //             else
+    //                 $scope.updateTotalsInfo(event.result);
 
-            } else {
-                dialogs.addNotify(event.error.errorMessage, "ERROR");
-            }
-            $scope.$apply();
-            busyWorker.hideBusy($element.find(".itemsTableContainer"));
-        });
-    };
+    //         } else {
+    //             dialogs.addNotify(event.error.errorMessage, "ERROR");
+    //         }
+    //         $scope.$apply();
+    //         busyWorker.hideBusy($element.find(".itemsTableContainer"));
+    //     });
+    // };
 
 }
 
