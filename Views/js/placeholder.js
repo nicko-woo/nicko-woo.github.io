33e53ded4125a5d35f2dfcd68101950e40ff67d1
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-07-15 14:11:40 +0300

    128

--- Views/js/placeholder.js
+++ Views/js/placeholder.js
@@ -1,6 +1,6 @@
 var PlaceHolder = function ($scope, $element) {
 
-    console.log("roos placeholder works 127");
+    console.log("roos placeholder works 128");
     this.getItems = function () {
         var items = [{
             text: "Remove Out Of Stock",
--- Views/simple/views/RemoveOutOfStock.js
+++ Views/simple/views/RemoveOutOfStock.js
@@ -1,43 +1,46 @@
 var RemoveOutOfStockView = function ($scope, $element, $filter, $compile, $q, controlService, stockService, purchaseorderService, $http, $timeout) {
 
+    $scope.itemsToRemove = $scope.$parent.$parent.$parent.order.Items;
     $scope.Initialize = function () {
-        //$scope.GetGridItemsToRemove();
+        $scope.GetGridItemsToRemove();
     };
 
     $scope.Initialize();
 
-    $scope.removeOutOfStock = function () {
-        console.log("items removed (test)");
-    };
+    // $scope.removeOutOfStock = function () {
+    //     console.log("items removed (test)");
+    // };
+
+
     
 
-    $scope.GetGridItemsToRemove = function () {
+    // $scope.GetGridItemsToRemove = function () {
 
-        let dataViewItemsToRemove = new Slick.Data.DataView();
+    //     let dataViewItemsToRemove = new Slick.Data.DataView();
 
-        let columnsItemsToRemove = [
-            { id: "column1", name: "SKU", field: "paymentDate", width: 220, cssClass: "slick-cell slickgrid-align-center", headerCssClass: "slick-header-column slickgrid-align-center" },
-            { id: "column2", name: "Quantity", field: "paidAmount", width: 220, cssClass: "slick-cell slickgrid-align-center", headerCssClass: "slick-header-column slickgrid-align-center" },
-            { id: "column3", name: "Unit Cost", field: "paidAmount", width: 220, cssClass: "slick-cell slickgrid-align-center", headerCssClass: "slick-header-column slickgrid-align-center" }
-        ];
+    //     let columnsItemsToRemove = [
+    //         { id: "column1", name: "SKU", field: "ChannelSKU", width: 220, cssClass: "slick-cell slickgrid-align-center", headerCssClass: "slick-header-column slickgrid-align-center" },
+    //         { id: "column2", name: "Quantity", field: "Quantity", width: 220, cssClass: "slick-cell slickgrid-align-center", headerCssClass: "slick-header-column slickgrid-align-center" },
+    //         { id: "column3", name: "Unit Cost", field: "Cost", width: 220, cssClass: "slick-cell slickgrid-align-center", headerCssClass: "slick-header-column slickgrid-align-center" }
+    //     ];
 
-        let optionsItemsToRemove = {
-            enableCellNavigation: true,
-            enableColumnReorder: false,
-            enableAutoResize: true,
-            forceFitColumns: true,
-            asyncEditorLoading: false,
-            autoEdit: false
-        };
+    //     let optionsItemsToRemove = {
+    //         enableCellNavigation: true,
+    //         enableColumnReorder: false,
+    //         enableAutoResize: true,
+    //         forceFitColumns: true,
+    //         asyncEditorLoading: false,
+    //         autoEdit: false
+    //     };
 
-        let data = [];
+    //     let data = $scope.data1;
 
-        if (data && data.length) {
-            dataViewItemsToRemove.setItems(data);
-        }
+    //     if (data && data.length) {
+    //         dataViewItemsToRemove.setItems(data);
+    //     }
 
-        $scope.gridItemsToRemove = new Slick.Grid("#roosItemsGrid", dataViewItemsToRemove, columnsItemsToRemove, optionsItemsToRemove);
-        $scope.gridItemsToRemove.setColumns(columnsItemsToRemove);
-    }
+    //     $scope.gridItemsToRemove = new Slick.Grid("#roosItemsGrid", dataViewItemsToRemove, columnsItemsToRemove, optionsItemsToRemove);
+    //     $scope.gridItemsToRemove.setColumns(columnsItemsToRemove);
+    // }
 
 };
