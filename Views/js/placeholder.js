dda5a4d3ffca4282020426affc31c0366070453b
M.Voievudskyi <m.voievudskyi@brainence.com>
2020-07-28 16:08:52 +0300

    179

--- Views/js/placeholder.js
+++ Views/js/placeholder.js
@@ -1,6 +1,6 @@
 var PlaceHolder = function ($scope, $element) {
 
-    console.log("roos placeholder works 178");
+    console.log("roos placeholder works 179");
     this.getItems = function () {
         var items = [{
             text: "Remove Out Of Stock",
