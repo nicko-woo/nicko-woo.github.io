5e1b3973c8d127c7031c6152416b039763b0d156
Mykola <m.voievudskyi@brainence.com>
2021-01-21 17:21:06 +0200

    added Kabir

---
+++ Kabir/Views/js/placeholder.js
@@ -0,0 +1,20 @@
+var PlaceHolder = function ($scope, $element) {
+
+    this.getItems = function () {
+        var items = [{
+            text: "Custom Images",
+            key: "customImagesTab",
+            icon: "fa fa-minus-circle"
+        }];
+
+        return items;
+    }
+
+    this.isEnabled = function (itemKey) {
+        return true;
+    }
+
+    
+}
+
+Core.PlaceHolderManager.register("MyInventory_EditInventoryItem_Tabs", PlaceHolder)
---
+++ Kabir/Views/simple/views/RemoveOutOfStock.js
@@ -0,0 +1,22 @@
+var RemoveOutOfStockView = function ($scope, $element, $filter, $compile, $q) {
+
+    var self = this;
+    self.onMessage = function (msg) {
+        switch (msg.key) {
+            case Core.Messenger.MESSAGE_TYPES.INITIALIZE:
+                $scope.itemsToRemove = msg.data.data.ItemsToRemove;
+        }
+    };
+
+    $scope.Close = function () {
+        $scope.itemsToRemove = null;
+        $scope.$destroy();
+        self.close();
+    }
+
+    $scope.isExists=function(type){
+        var obj = $scope.articles.find(function(x){ return x.type == type ; });
+        return obj !== null;
+      }
+
+};
