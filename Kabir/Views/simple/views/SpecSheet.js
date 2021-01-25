e34576d038482f4038d15b6319be449d98a138f1
Mykola <m.voievudskyi@brainence.com>
2021-01-25 18:34:48 +0200

    asdfadsfasfd

--- Kabir/Views/simple/views/SpecSheet.js
+++ Kabir/Views/simple/views/SpecSheet.js
@@ -25,22 +25,28 @@ var SpecSheetView = function ($scope, $element, $filter, $compile, $q) {
         } else {
           $scope.itemExtProps = event.result;
 
-          $scope.productDetails = $scope.itemExtProps.find(x => x.ProperyName === "Product Details");
-          $scope.artwork = $scope.itemExtProps.find(x => x.ProperyName === "Artwork");
-          $scope.packagingDetails = $scope.itemExtProps.find(x => x.ProperyName === "Packaging Details");
-          $scope.innerCarton = $scope.itemExtProps.find(x => x.ProperyName === "Inner Carton");
-          $scope.generalComments = $scope.itemExtProps.find(x => x.ProperyName === "General Comments");
+          $scope.productDetails = $scope.itemExtProps.find(
+            (x) => x.ProperyName === "Product Details"
+          );
+          $scope.artwork = $scope.itemExtProps.find(
+            (x) => x.ProperyName === "Artwork"
+          );
+          $scope.packagingDetails = $scope.itemExtProps.find(
+            (x) => x.ProperyName === "Packaging Details"
+          );
+          $scope.innerCarton = $scope.itemExtProps.find(
+            (x) => x.ProperyName === "Inner Carton"
+          );
+          $scope.generalComments = $scope.itemExtProps.find(
+            (x) => x.ProperyName === "General Comments"
+          );
         }
         Core.Dialogs.BusyWorker.hideBusy($element);
-
       }
     );
   };
 
-
-
   $scope.saveProperties = function () {
-
     var updatedProperties = [];
 
     updatedProperties.push($scope.productDetails);
@@ -50,37 +56,55 @@ var SpecSheetView = function ($scope, $element, $filter, $compile, $q) {
     updatedProperties.push($scope.generalComments);
 
     if (updatedProperties.length) {
-      $scope.inventoryService.UpdateInventoryItemExtendedProperties(updatedProperties, function (event) {
+      $scope.inventoryService.UpdateInventoryItemExtendedProperties(
+        updatedProperties,
+        function (event) {
           if (event.hasErrors()) {
             showErrorAndClose(event.error.errorMessage);
             Core.Dialogs.addNotify("ERROR", "ERROR");
           } else {
             Core.Dialogs.addNotify("Properties were updated", "SUCCESS");
           }
-      });
-  }
+        }
+      );
+    }
   };
 
   $scope.generateSpecSheet = function () {
-      // require dependencies
-      const PDFDocument = require(["pdfkit"]);
-      const blobStream = require(["blob-stream"]);
-      // create a document the same way as above
-      const doc = new PDFDocument();
-      // pipe the document to a blob
-      const stream = doc.pipe(blobStream());
-      // add your content to the document here, as usual
-      // get a blob when you're done
-      doc.end();
-      stream.on("finish", function () {
-        // get a blob you can do whatever you like with
-        const blob = stream.toBlob("application/pdf");
+    // // require dependencies
+    // const PDFDocument = require(["pdfkit"]);
+    // const blobStream = require(["blob-stream"]);
+    // // create a document the same way as above
+    // const doc = new PDFDocument();
+    // // pipe the document to a blob
+    // const stream = doc.pipe(blobStream());
+    // // add your content to the document here, as usual
+    // // get a blob when you're done
+    // doc.end();
+    // stream.on("finish", function () {
+    //   // get a blob you can do whatever you like with
+    //   const blob = stream.toBlob("application/pdf");
 
-        $scope.saveAs(blob, "SpecSheet.pdf");
-        // or get a blob URL for display in the browser
-        const url = stream.toBlobURL("application/pdf");
-        iframe.src = url;
-      });
+    //   $scope.saveAs(blob, "SpecSheet.pdf");
+    //   // or get a blob URL for display in the browser
+    //   const url = stream.toBlobURL("application/pdf");
+    //   iframe.src = url;
+    // });
+
+    var PdfPrinter = require(["pdfMake"]);
+    var printer = new PdfPrinter(fonts);
+    var fs = require(["fs"]);
+
+    var docDefinition = {
+      content: [
+        "First paragraph",
+        "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+      ],
+    };
+
+    var pdfDoc = printer.createPdfKitDocument(docDefinition);
+    pdfDoc.pipe(fs.createWriteStream("pdfs/basics.pdf"));
+    pdfDoc.end();
   };
 
   $scope.saveAs = function (blob, fileName) {
@@ -96,8 +120,8 @@ var SpecSheetView = function ($scope, $element, $filter, $compile, $q) {
 
     document.body.removeChild(anchorElem);
 
-    setTimeout(function() {
-        window.URL.revokeObjectURL(url);
+    setTimeout(function () {
+      window.URL.revokeObjectURL(url);
     }, 1000);
-}
+  };
 };
