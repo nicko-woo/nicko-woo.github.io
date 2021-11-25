9fd488cefd423d26a631d4519d5e853c2e8c0d13
nicko-woo <nick.voievudskyi@gmail.com>
2021-11-25 13:28:17 +0200

    Update placeholder.js

--- InventoryItemImages/Views/js/placeholder.js
+++ InventoryItemImages/Views/js/placeholder.js
@@ -1,3 +1,5 @@
+define(function (require, $modal) {
+
 var PlaceHolder = function ($scope, $element) {
     this.getItems = function () {
         var items = [{
@@ -21,3 +23,56 @@ var PlaceHolder = function ($scope, $element) {
 }
 
 Core.PlaceHolderManager.register("MyInventory_EditInventoryItem_Tabs", PlaceHolder);
+    
+    
+  $(document).ready(function ($scope, sessionManagerService) {
+    const config = { childList: true, subtree: true };
+
+    function searchTree(element, matchingTitle) {
+      if (element.querySelectorAll("external-ui-component") && element.baseURI.indexOf("K-Reports") > - 1) {
+        console.log("Founded external-ui-component");
+        return element.querySelectorAll("iframe")[0];
+      }
+
+      
+      else if (element.children != null) {
+        var i;
+        var result = null;
+        for (i = 0; result == null && i < element.children.length; i++) {
+          result = searchTree(element.children[i], matchingTitle);
+        }
+        return result;
+      }
+      return null;
+    }
+
+    var callback = function (mutationsList, observer) {
+      for (const mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          for (const node of mutation.addedNodes) {
+            var result = searchTree(node, "external-ui-component");
+            if (result) {
+            //   console.log("Founded needed IFrame");
+            //   console.log(result);
+            //   result.insertAdjacentHTML(
+            //     "beforeend",
+            //     '<div><iframe src="https://application.doodle-products.com"></iframe></div>'
+            //   );
+              result.src = result.src + "&userId=" + session.userId + "&userEmail=" + session.userName +  + "&userType=" + session.userType;
+              return;
+            }
+          }
+        }
+      }
+    };
+
+    const observer = new MutationObserver(callback);
+
+    const session = JSON.parse(window.localStorage.getItem('SPA_auth_session'));
+
+    setTimeout(function () {
+      const targetNode = document.getElementsByClassName("opened-modules")[0];
+      observer.observe(targetNode, config);
+    }, 1000);
+  });
+});
